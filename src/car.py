0f294893c02c9d6977ff5e80e6a58e1cd50cc300
lera <lavanda_ko@mail.ru>
2025-12-23 15:05:09 +0300

    ВСЕ ПРАВИЛЬНО

--- src/car.py
+++ src/car.py
@@ -1,22 +1,32 @@
+class TooMuchFuelError(Exception):
+    pass
+
+class NotEnoughFuelError(Exception):
+    pass
+
 class Car:
     def __init__(self, model: str, fuel_capacity: float) -> None:
         self._model = model
-        self._max_fuel_capacity: float = fuel_capacity
-        self._fuel_in_tank: float = 0
+        self._max_fuel_capacity = fuel_capacity
+        self._fuel_in_tank = 0
 
     def get_current_fuel_level(self) -> float:
         return self._fuel_in_tank
 
     def refuel_car(self, fuel_quantity: float):
+        too_much_fuel_msg = "Вы пытаетесь залить слишком много бензина!"
+
         if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
-            raise Exception("Вы пытаетесь залить слишком много бензина!")
+            raise TooMuchFuelError(too_much_fuel_msg)
         self._fuel_in_tank += fuel_quantity
 
     def drive(self, distance_km: float):
-        # Считаем, что расход 8 литров на 100 км
-        fuel_burned: int = 8 * (distance_km / 100)
-        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+        fuel_burned = 8 * (distance_km / 100)
+        not_enough_fuel_msg = "Не доедем жеж..."
+
         if self._fuel_in_tank < fuel_burned:
-            raise Exception("Не доедем жеж...")
+            raise NotEnoughFuelError(not_enough_fuel_msg)
+
         self._fuel_in_tank -= fuel_burned
         return self.get_current_fuel_level()
+
